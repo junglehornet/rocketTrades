703053be3cdab4566860afb4d12816910b9fac4e
JungleHornet <86211409+JungleHornet@users.noreply.github.com>
2025-11-30 18:00:53 -0600

    fixed issue with trades not getting loaded from file

--- common/src/main/java/games/luminance/rockettrades/RocketTrades.java
+++ common/src/main/java/games/luminance/rockettrades/RocketTrades.java
@@ -4,6 +4,7 @@ import com.google.gson.*;
 import com.google.gson.stream.MalformedJsonException;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import dev.architectury.platform.Platform;
+import dev.architectury.registry.level.entity.trade.TradeRegistry;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.item.ItemStack;
@@ -24,6 +25,19 @@ public final class RocketTrades {
     private static String json = "";
 
     private static void save() throws IOException, CommandSyntaxException {
+
+        updateJson();
+        try {
+            FileWriter jsonWriter = new FileWriter(Platform.getConfigFolder().toString() + "/rockettrades/trades.json");
+            jsonWriter.write(json);
+            jsonWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error: RocketTrades failed to save trades to disk.");
+            e.printStackTrace();
+        }
+    }
+
+    private static void createTradeFile() {
         if (!jsonDir.toFile().exists() || jsonDir.toFile().exists() && jsonDir.toFile().isDirectory()) {
             if (jsonDir.toFile().isDirectory()) {
                 jsonDir.toFile().delete();
@@ -46,32 +60,10 @@ public final class RocketTrades {
                 } catch (IOException e) {
                     System.out.println("Error: RocketTrades failed to save.");
                     e.printStackTrace();
-                    return;
                 }
             } catch (Exception e) {
                 e.printStackTrace();
-                return;
             }
-        } else {
-            if (json.isEmpty()) {
-                Scanner jsonReader = new Scanner(jsonPath.toFile());
-                StringBuilder jsonBuilder = new StringBuilder();
-                while (jsonReader.hasNextLine()) {
-                    jsonBuilder.append(jsonReader.nextLine());
-                }
-                jsonReader.close();
-                json = jsonBuilder.toString();
-            }
-        }
-
-        updateJson();
-        try {
-            FileWriter jsonWriter = new FileWriter(Platform.getConfigFolder().toString() + "/rockettrades/trades.json");
-            jsonWriter.write(json);
-            jsonWriter.close();
-        } catch (IOException e) {
-            System.out.println("Error: RocketTrades failed to save trades to disk.");
-            e.printStackTrace();
         }
     }
 
@@ -81,10 +73,6 @@ public final class RocketTrades {
             jsonArray.add(trade.getJson());
         }
         json = jsonArray.toString();
-    }
-
-    public static ArrayList<RocketTrade> getTradeList() {
-        return tradeList;
     }
 
     public static String[] getNameList() {
@@ -103,6 +91,7 @@ public final class RocketTrades {
             throw new RuntimeException(e);
         }
         trade.registerTrade();
+        System.out.printf("--RocketTrades registered new trade: %s\n", trade.getName());
     }
 
     public static boolean exists(String name) {
@@ -121,7 +110,7 @@ public final class RocketTrades {
         return new RocketTrade(new ItemStack(Items.EMERALD, 1), rockets, 7, 8, 0.02f, 1, VillagerProfession.CARTOGRAPHER, "Rocket Trade");
     }
 
-    private static void loadTradesFromJson() throws FileNotFoundException, CommandSyntaxException {
+    private static void loadTradesFromJson() throws CommandSyntaxException, FileNotFoundException {
         Scanner jsonReader = new Scanner(jsonPath.toFile());
         StringBuilder jsonBuilder = new StringBuilder();
         while (jsonReader.hasNextLine()) {
@@ -132,23 +121,24 @@ public final class RocketTrades {
         if (json.isEmpty()) json = "[]";
 
         JsonArray tradesGSON = JsonParser.parseString(json).getAsJsonArray();
-        Object[] trades = tradesGSON.asList().toArray();
 
-        for (Object o : trades) {
+        System.out.println("====  RocketTrades loading trades...  ====");
+        for (JsonElement o: tradesGSON) {
             RocketTrade trade = RocketTrade.fromString(o.toString());
             if (trade != null) {
                 tradeList.add(trade);
                 trade.registerTrade();
+                System.out.printf("--Trade Registered: %s\n", trade.getName());
             }
         }
+        System.out.println("==== RocketTrades done loading trades ====");
     }
 
     public static void init() throws CommandSyntaxException, IOException {
         // Write common init code here.
-        save();
+        createTradeFile();
+        loadTradesFromJson();
 
         RocketTradesCommands.register();
-
-        loadTradesFromJson();
     }
 }
